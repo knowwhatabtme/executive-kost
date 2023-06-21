b24857672987a14918ed2b2b5f6d92af3b4ad0e1
And Than <andenn@student.gunadarma.ac.id>
2023-06-21 18:40:07 +0700

    FE + BE Finall 1.9

--- resources/views/frontend/blog/partials/modal.blade.php
+++ resources/views/frontend/blog/partials/modal.blade.php
@@ -5,7 +5,8 @@
                 <h1 class="modal-title fs-5" id="exampleModalLabel">Filter</h1>
             </div>
             <div class="modal-body">
-                <button type="button" class="btn btn-secondary">Secondary</button>
+                <button type="button" class="btn btn-secondary">Limo</button>
+                <button type="button" class="btn btn-secondary">PD Labu</button>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
